ca3b48b985acefd838769ec6d332c060dd37e459
tonykosseify123 <68563002+Tonykosseify@users.noreply.github.com>
2022-01-10 20:41:05 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+
+const App = () => {
+    return (
+        <div>
+            hey it is working 
+        </div>
+    )
+};
+
+export default App;
---
+++ src/components/Phone/Phone.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Phone.css";
+
+const Phone = () => {
+    return (
+        <div className="phone">
+            
+        </div>
+    );
+};
+
+
+export default Phone ;
